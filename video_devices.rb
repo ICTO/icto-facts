48113422e944e313d0bd502aa17b9c8788106098
Kristof Keppens <kkeppens@gmail.com>
2013-04-29 14:04:11 +0200

    report the card title in addition to driver and driver version

--- video_devices.rb
+++ video_devices.rb
@@ -6,22 +6,26 @@ Dir.chdir('/dev')
 devices = Dir.glob('video[0-9]')
 devices.each_with_index do |dev, i|
   
-  Facter.add("#{dev}driver") do
+  Facter.add("#{dev}card") do
     setcode do
-      Facter::Util::Resolution.exec("v4l-info /dev/#{dev} | awk 'driver' | awk -F '\"' '{print $2}'")
+      Facter::Util::Resolution.exec("v4l-info /dev/#{dev} | awk '/card/' | awk -F '\"' '{print $2}'")
     end
   end
 
-  output = `v4l-info /dev/#{dev} | awk 'version' | awk -F ':' '{print $2}'`
+  Facter.add("#{dev}driver") do
+    setcode do
+      Facter::Util::Resolution.exec("v4l-info /dev/#{dev} | awk '/driver/' | awk -F '\"' '{print $2}'")
+    end
+  end
 
   Facter.add("#{dev}driverversion") do
     setcode do
-      output
+      Facter::Util::Resolution.exec("v4l-info /dev/#{dev} | awk '/version/' | awk -F ':' '{print $2}'")
     end
   end
 end
 
-count = Dir.glob('video*').count()
+count = Dir.glob('video[0-9]').count()
 
 Facter.add("videodevicescount") do
   setcode do
