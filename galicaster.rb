57d40f61f1786577a080cdfbb85303f198ddf3b6
Kristof Keppens <kkeppens@gmail.com>
2013-05-16 10:49:23 +0200

    add specific facts about camera and screen device used in galicaster

--- galicaster.rb
+++ galicaster.rb
@@ -1,11 +1,39 @@
 #
 # galicaster.rb Custom facts about galicaster capture agent
 #
- 
+
 if File.exists?("/home/icto/Galicaster/galicaster/__init__.py") then
   Facter.add(:galicaster_version) do
     setcode do
       Facter::Util::Resolution.exec('awk \'/__version__/ {print $3}\' /home/icto/Galicaster/galicaster/__init__.py | sed "s/\'//g"')
     end
-  end 
+  end
+
+  if File.exists?("/dev/screen") then
+    Facter.add(:screendriver) do
+      setcode do
+        Facter::Util::Resolution.exec("v4l-info /dev/screen | awk '/driver/' | awk -F '\"' '{print $2}'")
+      end
+    end
+
+    Facter.add(:screenversion) do
+      setcode do
+        Facter::Util::Resolution.exec("v4l-info /dev/screen | awk '/version/' | awk -F '\"' '{print $2}'")
+      end
+    end
+  end
+
+  if File.exists?("/dev/camera") then
+    Facter.add(:cameradriver) do
+      setcode do
+        Facter::Util::Resolution.exec("v4l-info /dev/camera | awk '/driver/' | awk -F '\"' '{print $2}'")
+      end
+    end
+
+    Facter.add(:cameraversion) do
+      setcode do
+        Facter::Util::Resolution.exec("v4l-info /dev/camera | awk '/version/' | awk -F '\"' '{print $2}'")
+      end
+    end
+  end
 end
