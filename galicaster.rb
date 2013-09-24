3dcc5e947f3497b553b51fa4b42e5d92acf07f46
Kristof Keppens <kkeppens@gmail.com>
2013-09-24 16:05:39 +0200

    forgot path

--- galicaster.rb
+++ galicaster.rb
@@ -5,7 +5,7 @@
 if File.exists?("/usr/share/galicaster/galicaster/__init__.py") then
   Facter.add(:galicaster_version) do
     setcode do
-      Facter::Util::Resolution.exec('awk \'/__version__/ {print $3}\' /home/icto/Galicaster/galicaster/__init__.py | sed "s/\'//g"')
+      Facter::Util::Resolution.exec('awk \'/__version__/ {print $3}\' /usr/share/galicaster/galicaster/__init__.py | sed "s/\'//g"')
     end
   end
 
