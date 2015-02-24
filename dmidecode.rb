be0305af669c0b00ce0c2abad0e9db8b7afecdcb
Kristof Keppens <kkeppens@gmail.com>
2015-02-24 11:55:22 +0100

    info already present in facter

--- dmidecode.rb
+++ dmidecode.rb
@@ -1,7 +1,7 @@
 if FileTest.exists?("/usr/sbin/dmidecode")
 
   # Add remove things to query here
-  query = { 'BIOS Information' => ['Vendor:', 'Version:', 'Release Date:'], 'System Information' =>  [ 'Manufacturer:', 'Product Name:' , 'Serial Number:', 'Version:' ], 'Chassis Information' => 'Type:', 'Processor Information' => ['Version:', 'Max Speed:'], 'Memory Controller Information' => [ 'Maximum Memory Module Size:', 'Maximum Total Memory Size:' ] }
+  query = { 'BIOS Information' => 'Vendor:', 'System Information' =>  [ 'Manufacturer:', 'Product Name:' , 'Serial Number:', 'Version:' ], 'Chassis Information' => 'Type:', 'Processor Information' => ['Version:', 'Max Speed:'], 'Memory Controller Information' => [ 'Maximum Memory Module Size:', 'Maximum Total Memory Size:' ] }
 
   # Run dmidecode only once
   output=%x{/usr/sbin/dmidecode 2>/dev/null}
