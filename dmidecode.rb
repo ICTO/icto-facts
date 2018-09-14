bf38cdd910de039c2c10327258975b56565a0e63
Kristof Keppens <kkeppens@gmail.com>
2018-09-14 11:05:47 +0200

    execute with sudo

--- dmidecode.rb
+++ dmidecode.rb
@@ -4,7 +4,7 @@ if FileTest.exists?("/usr/sbin/dmidecode")
   query = { 'BIOS Information' => ['Vendor:'], 'System Information' =>  [ 'Manufacturer:', 'Product Name:' , 'Serial Number:', 'Version:' ], 'Chassis Information' => ['Type:'], 'Processor Information' => ['Version:', 'Max Speed:'], 'Memory Controller Information' => [ 'Maximum Memory Module Size:', 'Maximum Total Memory Size:' ] }
 
   # Run dmidecode only once
-  output=%x{/usr/sbin/dmidecode 2>/dev/null}
+  output=%x{sudo /usr/sbin/dmidecode 2>/dev/null}
 
   query.each_pair do |key,v|
     v.each do |value|
@@ -23,12 +23,12 @@ if FileTest.exists?("/usr/sbin/dmidecode")
   end
   Facter.add(:biosversion) do
     setcode do
-      Facter::Util::Resolution.exec("/usr/sbin/dmidecode -s bios-version")
+      Facter::Util::Resolution.exec("sudo /usr/sbin/dmidecode -s bios-version")
     end
   end
   Facter.add(:biosreleasedate) do
     setcode do
-      Facter::Util::Resolution.exec("/usr/sbin/dmidecode -s bios-release-date")
+      Facter::Util::Resolution.exec("sudo /usr/sbin/dmidecode -s bios-release-date")
     end
   end
 end
